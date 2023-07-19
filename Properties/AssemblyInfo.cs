b2587449296aebbf6c46a795dc9f03126804ef24
Dyvine57 <dulana57@gmail.com>
2023-07-19 14:00:46 -0400

    version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Windows;
 
 [assembly: PluginDisplayName("EffectBlueprint Editor")]
 [assembly: PluginAuthor("Dyvinia")]
-[assembly: PluginVersion("1.2.3")]
+[assembly: PluginVersion("1.3.0")]
 
 [assembly: RegisterAssetDefinition("EffectBlueprint", typeof(EmitterDocumentDefinition))]
