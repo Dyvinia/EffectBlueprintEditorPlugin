a51eaa7afa7f0b01ad24cc82c7073e07e09b7690
Dyvinia <dulana57@gmail.com>
2024-10-19 15:52:11 -0400

    remove unused usings

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,9 +1,7 @@
-﻿using EffectBlueprintEditorPlugin;
-using Frosty.Core.Attributes;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
+using Frosty.Core.Attributes;
+using EffectBlueprintEditorPlugin;
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
